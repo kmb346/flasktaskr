d72a1d9b3f7549300a614555e6a516363085815f
Behrens Kevin <kmb346@yahoo.com>
2014-12-20 08:34:40 +0900

    Switched database from sqlite format to sqlalchemy

--- db_create.py
+++ db_create.py
@@ -1,28 +1,17 @@
 # db_create.py
 
 
-import sqlite3 
-from config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
+# create the database and the db table
+db.create_all()
 
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
-	
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY
-	    AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority
-		   INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
-		
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks(name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "12/31/2014", 10, 1)'
-    )
-	
-    c.execute(
-        'INSERT INTO tasks(name, due_date, priority, status)'
-    	'VALUES("Finish Real Python Course 2", "12/31/2014", 10, 1)'
-    )
+#insert database
+db.session.add(Task("Finish this tutorial", date(2014, 12, 31), 10, 1))
+db.session.add(Task("Finish Real Python", date(2014, 12, 31), 10, 1))
+
+# commit the changes
+db.session.commit()
+
