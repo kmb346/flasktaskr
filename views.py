8fc015f5d822c986876ad1a80aff4a44cd99fc41
Behrens Kevin <kmb346@yahoo.com>
2014-12-21 18:07:05 +0900

    updating forms for errors, completed register.html, started login.html

--- views.py
+++ views.py
@@ -140,5 +140,11 @@ def register():
                 error=error)
     if request.method == 'GET':
         return render_template('register.html', form=form)
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" %(
+                getattr(form,field).label.text, error), 'error')
 		
 	
